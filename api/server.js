b807e803d23513fe2bac40bfec92bf5d9695096f
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-07-29 11:19:27 -0300

    feat(api): add server.js file and update package.json and vercel.json

---
+++ api/server.js
@@ -0,0 +1,17 @@
+import jsonServer from 'json-server'
+
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+}))
+server.use(router)
+
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
+
+export { server }
