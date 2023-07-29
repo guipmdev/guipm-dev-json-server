5c21ae728b973c1099c5cb9e0be4095f2106612e
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-07-29 11:43:26 -0300

    feat(server): add nodemon script for development

--- api/server.js
+++ api/server.js
@@ -2,16 +2,19 @@ import jsonServer from 'json-server'
 
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults({
+  readOnly: true,
+})
 
 server.use(middlewares)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-}))
-server.use(router)
 
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1',
+}))
+
+server.use(router)
 server.listen(3000, () => {
-    console.log('JSON Server is running')
+  console.log('JSON Server is running')
 })
 
 export { server }
